8a167355781d81db6ccee841d64f56a48597a13c
Will Enright <wenright0@gmail.com>
2024-09-21 19:16:29 -0500

    Shrink down fonts and margins

--- src/components/item.tsx
+++ src/components/item.tsx
@@ -48,7 +48,7 @@ const Item = (props: ItemProps) => {
 
   if (!item || name === 'empty') {
     return (
-      <div className={`w-[64px] h-[64px] m-1 text-white rounded-md group/item flex items-center justify-center cursor-pointer bg-zinc-500`}></div>
+      <div className={`text-white rounded-md group/item flex items-center justify-center cursor-pointer bg-zinc-500`}></div>
     );
   }
 
@@ -57,40 +57,40 @@ const Item = (props: ItemProps) => {
   const itemType: 'Weapon' | 'Armor' | 'Tech' = item.Slot;
   
   return (
-    <div className={`w-[64px] h-[64px] m-1 text-lg text-white rounded-md group/item flex items-center justify-center cursor-pointer ${colorLightBg[itemType]} ${colorDarkDefaultBg[itemType]}`}>
-      <img className="w-12 h-12 opacity-75 invert" src={"/item_images/" + name + ".png"} />
+    <div className={`text-sm text-white rounded-md group/item flex items-center justify-center cursor-pointer ${colorLightBg[itemType]} ${colorDarkDefaultBg[itemType]}`}>
+      <img className="w-6 h-6 p-0.5 opacity-75 invert" src={"/item_images/" + name + ".png"} />
       {item &&
-        <div className={`absolute left-0 right-0 w-[320px] z-30 overflow-hidden rounded-md mx-1 my-4 bottom-full hidden group-hover/item:block text-white/65 ${colorLightBg[itemType]}`}>
-          <div className="p-4">
-            <div className="text-3xl text-white capitalize">{friendlyName}</div>
+        <div className={`absolute left-0 right-0 w-[320px] z-30 overflow-hidden rounded-md mx-0.5 my-4 bottom-full hidden group-hover/item:block text-white/65 ${colorLightBg[itemType]}`}>
+          <div className="p-2">
+            <div className="text-xl text-white capitalize">{friendlyName}</div>
             <div>
               <img src="/souls_icon.png" alt="" className="inline w-5 p-1" />
               <div className="inline text-white">{item.Cost}</div>
             </div>
           </div>
-          <div className={`py-4 ${colorDarkBg[itemType]}`}>
+          <div className={`py-2 ${colorDarkBg[itemType]}`}>
             {Object.entries(simpleUpgrades)
               .filter(([upgradeRealName, upgradeSpec]: [string, UpgradeSpec]) => isTangible(upgradeRealName))
               .map(([upgradeRealName, upgradeSpec]: [string, UpgradeSpec]) => (
-                <div key={upgradeRealName} className={`px-4`}>{upgradeSpec.signPrefix}<p className="inline text-white">{item[upgradeRealName]}</p>{upgradeSpec.signSuffix} {upgradeSpec.readableName}</div>
+                <div key={upgradeRealName} className={`px-2`}>{upgradeSpec.signPrefix}<p className="inline text-white">{item[upgradeRealName]}</p>{upgradeSpec.signSuffix} {upgradeSpec.readableName}</div>
             ))}
           </div>
-          <div className={`text-sm flex`}>
-            <div className={`p-2 px-4 italic w-full ${colorDarkerBg[itemType]}`}>{item.Activation}</div>
+          <div className={`text-sm flex items-center ${colorDarkerBg[itemType]}`}>
+            <div className={`p-1 px-2 italic w-full`}>{item.Activation}</div>
             {item.Cooldown && item.Cooldown !== '0' &&
               <div className="flex p-2 bg-black opacity-90"><img className="mx-2" src="/clock-arrow.svg" width={16} height={16} alt="" /><p className="mr-6">{item.Cooldown}s</p></div>
             }
           </div>
           {item.Description &&
-            <div className={`p-2 px-4 ${colorDarkBg[itemType]}`} dangerouslySetInnerHTML={{__html: item.Description}}></div>
+            <div className={`p-1 px-2 ${colorDarkBg[itemType]}`} dangerouslySetInnerHTML={{__html: item.Description}}></div>
           }
           {Object.keys(conditionalUpgrades).some(upgrade => item[upgrade] && item[upgrade] !== '0') &&
-            <div className={`p-2 ${colorDarkBg[itemType]}`}>
-              <div className={`rounded-sm p-2 ${colorDarkerBg[itemType]}`}>
+            <div className={`p-1 ${colorDarkBg[itemType]}`}>
+              <div className={`rounded-sm p-1 m-1 ${colorDarkerBg[itemType]}`}>
                 {Object.entries(conditionalUpgrades)
                   .filter(([upgradeRealName, upgradeSpec]: [string, UpgradeSpec]) => isTangible(upgradeRealName))
                   .map(([upgradeRealName, upgradeSpec]: [string, UpgradeSpec]) => (
-                    <div key={upgradeRealName} className={`px-4`}>{upgradeSpec.signPrefix}<p className="inline text-white">{item[upgradeRealName]}</p>{upgradeSpec.signSuffix} {upgradeSpec.readableName}</div>
+                    <div key={upgradeRealName} className={`px-2`}>{upgradeSpec.signPrefix}<p className="inline text-white">{item[upgradeRealName]}</p>{upgradeSpec.signSuffix} {upgradeSpec.readableName}</div>
                 ))}
               </div>
             </div>
